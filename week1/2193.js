045fcddc6af75b96b760e6acb7e665437c7a1f4b
sungminyun1 <smy07207@gmail.com>
2022-05-21 01:03:34 +0900

    2193

--- week1/2193.js
+++ week1/2193.js
@@ -1,10 +1,14 @@
-1
 
-10
+const input = `3`;
+main(+input)
+function main(N){
+    const memo = [[0,0],[0,1],[1,0]]
 
-100
-101
+    for(let i =3; i<=N; i++){
+        let zero = BigInt(memo[i-1][0]) + BigInt(memo[i-1][1]);
+        let one = BigInt(memo[i-1][0])
+        memo[i] = [zero,one]
+    }
 
-1000
-1001
-1010
+    console.log((memo[N][0] + memo[N][1]).toString())
+}
