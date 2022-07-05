522d9594225c701282db6ae697c6da501e6b529e
sungminyun1 <smy07207@gmail.com>
2022-07-06 00:00:54 +0900

    1182

--- 540/1182.js
+++ 540/1182.js
@@ -9,20 +9,27 @@ main(N,S,input[1])
 
 function main(N,S,Arr){
     let count = 0;
-
-    const find = (depth,start,val) =>{
-        if(depth === N+1){
-            return;
+    const used = new Array(N).fill(false)
+    const find = (depth,t_depth,start,val) =>{
+        if(depth === t_depth){
+            return
         }
         if(val === S){
-            count++
+            count++;
         }
-
-        find(now+1,val + Arr[now+1])
+        for(let i = start; i<Arr.length; i++){
+            if(!used[i]){
+                used[i] = true;
+                find(depth +1, t_depth, i, val+Arr[i]);
+                used[i] = false
+            }
+        }
     }
 
     for(let i =0; i<Arr.length; i++){
-        find(0,i,Arr[i])
+        used[i] = true
+        find(0,N-i,i,Arr[i])
+        used[i] = false
     }
 
     console.log(count)
