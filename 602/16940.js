94b39837bb8d0a451fdcbeceedf3731799a3e3be
sungminyun1 <smy07207@gmail.com>
2022-08-02 00:19:07 +0900

    16940

--- 602/16940.js
+++ 602/16940.js
@@ -2,7 +2,10 @@ const input = `4
 1 2
 1 3
 2 4
-1 2 3 4`.split('\n');
+1 2 4 3`.split('\n');
+
+const fs = require("fs");
+const input = fs.readFileSync("dev/stdin").toString().trim().split('\n')
 
 const N = +input.shift();
 const graph = [];
@@ -17,6 +20,31 @@ for(let i =0; i<input.length-1; i++){
     graph[to].push(from);
 }
 main(N,graph,input.pop().split(' ').map(Number))
+
 function main(N,Graph,Target){
-    console.log(N,Graph,Target)
+    const needVisit = [Graph[1]];
+    const visited = [false,true]
+
+
+    let idx = 0;
+    while(idx<Target.length){
+        let candi = needVisit.shift();
+        for(let i =0; i<candi.length; i++){
+            idx++;
+            if(!Target[idx]){
+                console.log(1)
+                return;
+            }
+            if(!candi.includes(Target[idx])){
+                console.log(0)
+                return;
+            }
+            if(!visited[Target[idx]]){
+                needVisit.push(Graph[Target[idx]])
+                visited[Target[idx]] = true;
+            }
+        }
+    }
+
+    
 }
