d4260f729deb9adb1ea9d33f82ad36c155be9b8a
sungminyun1 <smy07207@gmail.com>
2022-08-03 23:34:53 +0900

    08.03

--- 602/16940.js
+++ 602/16940.js
@@ -4,8 +4,8 @@ const input = `4
 2 4
 1 2 4 3`.split('\n');
 
-const fs = require("fs");
-const input = fs.readFileSync("dev/stdin").toString().trim().split('\n')
+// const fs = require("fs");
+// const input = fs.readFileSync("dev/stdin").toString().trim().split('\n')
 
 const N = +input.shift();
 const graph = [];
@@ -21,30 +21,32 @@ for(let i =0; i<input.length-1; i++){
 }
 main(N,graph,input.pop().split(' ').map(Number))
 
-function main(N,Graph,Target){
-    const needVisit = [Graph[1]];
-    const visited = [false,true]
+// function main(N,Graph,Target){
+//     const needVisit = [Graph[1]];
+//     const visited = [false,true]
 
 
-    let idx = 0;
-    while(idx<Target.length){
-        let candi = needVisit.shift();
-        for(let i =0; i<candi.length; i++){
-            idx++;
-            if(!Target[idx]){
-                console.log(1)
-                return;
-            }
-            if(!candi.includes(Target[idx])){
-                console.log(0)
-                return;
-            }
-            if(!visited[Target[idx]]){
-                needVisit.push(Graph[Target[idx]])
-                visited[Target[idx]] = true;
-            }
-        }
-    }
+//     let idx = 0;
+//     while(idx<Target.length){
+//         let candi = needVisit.shift();
+//         for(let i =0; i<candi.length; i++){
+//             idx++;
+//             if(!Target[idx]){
+//                 console.log(1)
+//                 return;
+//             }
+//             if(!candi.includes(Target[idx])){
+//                 console.log(0)
+//                 return;
+//             }
+//             if(!visited[Target[idx]]){
+//                 needVisit.push(Graph[Target[idx]])
+//                 visited[Target[idx]] = true;
+//             }
+//         }
+//     }
 
-    
-}
+
+
+
+// }
