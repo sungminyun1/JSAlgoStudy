69b7b28f21875b0171d60d6fc735c31ce3dbec09
sungminyun1 <smy07207@gmail.com>
2022-08-20 19:00:03 +0900

    0820

--- 602/2146.js
+++ 602/2146.js
@@ -10,27 +10,27 @@ const input = `10
 0 0 0 0 1 1 1 0 0 0
 0 0 0 0 0 0 0 0 0 0`.split('\n');
 
+// const fs = require("fs");
+// const input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
+
 main(+input.shift(),input.map(val=>val.split(' ').map(Number)))
 
 function main(N,Board){
     let token = 0;
     const dir = [[0,1],[1,0],[-1,0],[0,-1]]
+    const land = [];
 
     let min = Infinity;
 
-    const bfs = (row,col) =>{
-        const needVisit = [[row,col]]
-        
-        while(needVisit.length){
-            const [nowr,nowc] = needVisit.shift();
-            if(Board[nowr][nowc] === token) continue;
-            Board[nowr][nowc] = token;
-            for(let i =0; i<dir.length; i++){
-                const nexr = nowr + dir[i][0];
-                const nexc = nowc + dir[i][1];
-                if(nexr >=0 && nexr < N && nexc >=0 && nexc <N && Board[nexr][nexc] === 1){
-                    needVisit.push([nexr,nexc])
-                }
+    const dfs = (nowr,nowc) =>{
+        if(Board[nowr][nowc] === token) return;
+        Board[nowr][nowc] = token;
+        land[token*(-1)].push([nowr,nowc,0])
+        for(let i =0; i<dir.length; i++){
+            const nexr = nowr + dir[i][0];
+            const nexc = nowc + dir[i][1];
+            if(nexr >=0 && nexr < N && nexc >=0 && nexc <N && Board[nexr][nexc] === 1){
+                dfs(nexr,nexc)
             }
         }
     }
@@ -38,6 +38,7 @@ function main(N,Board){
     const find = (start,needVisit,visited) =>{
         while(needVisit.length){
             let [row,col,dist] = needVisit.shift();
+            if(visited[row][col]) continue;
             visited[row][col] = true;
 
             for(let i =0; i<dir.length; i++){
@@ -58,36 +59,18 @@ function main(N,Board){
         for(let j =0; j<N; j++){
             if(Board[i][j] === 1){
                 token--;
-                bfs(i,j)
+                land[token*(-1)] = [];
+                dfs(i,j)
             }
         }
     }
 
-    for(let i = -1; i>=token; i--){
+    for(let i = 1; i<land.length; i++){
         let v =[];
         for(let j =0; j<N; j++){
             v.push([]);
         }
-        let nv = [];
-        for(let j=0; j<N; j++){
-            for(let k=0; k<N; k++){
-                if(Board[j][k] === i) nv.push([j,k])
-            }
-        }
-        let nv2 = [];
-        for(let j=0; j<nv.length; j++){
-            let [nrow,ncol] = nv[j];
-            for(let k =0; k<dir.length; k++){
-                let nexrow = nrow + dir[k][0];
-                let nexcol = ncol + dir[k][1];
-                if(nexrow >=0 && nexrow<N && nexcol >=0 && nexcol<N && Board[nexrow][nexcol] === 0){
-                    nv2.push([nexrow,nexcol,1])
-                }
-            }
-        }
-
-        find(i,nv2,v)
+        find(i*(-1),land[i],v)
     }
-
     console.log(min)
 }
