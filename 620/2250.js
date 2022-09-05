beefb01256eb4b3e10dbe2adb75eb7cf851603d5
sungminyun1 <smy07207@gmail.com>
2022-09-06 00:40:43 +0900

    2250

--- 620/2250.js
+++ 620/2250.js
@@ -19,13 +19,63 @@ const input = `19
 18 -1 -1
 19 -1 -1`.split('\n');
 
+// const fs = require("fs");
+// const input = fs.readFileSync("dev/stdin").toString().trim().split('\n')
+
 const N = +input.shift();
 
 main(N,input.map(val=>val.split(' ').map(Number)))
 
 function main(N,Graph){
-    console.log(N,Graph)
+    const rc = new Array(N+1).fill(0);
+    const graph = [];
 
+    for(let i =0; i<Graph.length; i++){
+        graph[Graph[i][0]] = [Graph[i][1],Graph[i][2]]
+        for(let j =0; j<Graph[i].length; j++){
+            if(Graph[i][j] !== -1){
+                rc[Graph[i][j]]++
+            }
+        }
+    }
 
-    
+    const root = rc.indexOf(1)
+    const his = [];
+
+    const find = (now,level) =>{
+        const [left,right] = graph[now];
+        if(left !== -1){
+            find(left,level+1)
+        }
+        his.push(level)
+        if(right !== -1){
+            find(right,level+1)
+        }
+        
+    }
+
+    find(root,1)
+
+    let max = [root,1]
+    const check = [];
+    for(let i=0; i<his.length; i++){
+        let now = his[i]
+        if(!check[now]){
+            for(let j = his.length-1; j>=i; j--){
+                if(now === his[j]){
+                    let dist = j-i+1;
+
+                    if(dist > max[1]){
+                        max[0] = now;
+                        max[1] = dist;
+                    }else if(dist === max[1] && now < max[0]){
+                        max[0] = now;
+                    }
+                }
+            }
+        }
+        check[now] = true;
+    }
+
+    console.log(max.join(' '))
 }
