9549da05c0ed708ee5404d6676c77950e254c12e
unknown <smy07207@gmail.com>
2022-09-09 17:05:03 +0900

    1167

--- 620/1167.js
+++ 620/1167.js
@@ -1,9 +1,10 @@
-const input = `5
-1 3 2 -1
-2 4 4 -1
-3 1 2 4 3 -1
-4 2 4 3 3 5 6 -1
-5 4 6 -1`.split('\n');
+const input = `6
+1 2 3 -1
+2 1 3 5 3 3 5 -1
+3 2 5 4 7 -1
+4 3 7 -1
+5 2 3 6 5 -1
+6 5 5 -11`.split('\n');
 
 // const fs = require("fs");
 // const input = fs.readFileSync("dev/stdin").toString().trim().split('\n')
@@ -25,26 +26,31 @@ for(let i =0; i<input.length; i++){
 }
 
 const root = rc.indexOf(1)
+let max = [0];
 
-let max = 0;
-
-const needVisit = [[root,0]];
-const visited = []
-while(needVisit.length){
-    const [now,dist] = needVisit.shift();
-    if(visited[now]) continue;
-
-    visited[now] = true;
-    if(max<dist){
-        max = dist;
-    }
-
-    for(let i =0; i<graph[now].length; i++){
-        const [next,len] = graph[now][i];
-        if(!visited[next]){
-            needVisit.push([next,dist+len])
+const find = (start) =>{
+    const needVisit = [[start,0]];
+    const visited = []
+    while(needVisit.length){
+        const [now,dist] = needVisit.shift();
+        if(visited[now]) continue;
+    
+        visited[now] = true;
+        if(max[0]<dist){
+            max[0] = dist;
+            max[1] = now;
+        }
+    
+        for(let i =0; i<graph[now].length; i++){
+            const [next,len] = graph[now][i];
+            if(!visited[next]){
+                needVisit.push([next,dist+len])
+            }
         }
     }
 }
 
-console.log(max)
+find(root)
+find(max[1])
+
+console.log(max[0])
