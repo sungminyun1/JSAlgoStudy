593088186cd5b6c023e34e5c07cf78a73ae1ebab
unknown <smy07207@gmail.com>
2022-10-19 21:57:29 +0900

    1062

--- 541/1062.js
+++ 541/1062.js
@@ -1,7 +1,16 @@
-const input = `3 6
-antarctica
-antahellotica
-antacartica`.split('\n');
+const input = `9 8
+antabtica
+antaxtica
+antadtica
+antaetica
+antaftica
+antagtica
+antahtica
+antajtica
+antaktica`.split('\n');
+
+// const fs = require("fs");
+// const input = fs.readFileSync("dev/stdin").toString().trim().split('\n')
 
 const [N,K] = input.shift().split(' ').map(Number);
 const Arr = input.map(val=>val.split(''))
@@ -15,35 +24,44 @@ if(K<5){
     console.log(0)
     return;
 }
+if(K===26){
+    console.log(N);
+    return;
+}
 let max = 0;
 const pick = [];
-const find = (level) =>{
-    if(level === K){
-        // let count = 0 ;
-        // for(let i=0; i<Arr.length; i++){
-        //     let correct = true
-        //     for(let j =0; j<Arr[i].length; i++){
-        //         if(!pick[alpha[Arr[i][j]]]){
-        //             correct= false;
-        //             break;
-        //         }
-        //     }
-        //     if(correct) count++
-        // }
-        // if(count>max) max = count;
-        max++
+pick[1] = true;
+pick[3] = true;
+pick[9] = true;
+pick[14] = true;
+pick[20] = true
+
+const find = (level,start) =>{
+    if(level === K-5){
+        let count = 0 ;
+        for(let i=0; i<Arr.length; i++){
+            let correct = true
+            for(let j =0; j<Arr[i].length; j++){
+                if(!pick[alpha[Arr[i][j]]]){
+                    correct= false;
+                    break;
+                }
+            }
+            if(correct) count++
+        }
+        if(count>max) max = count;
         return;
     }
-    for(let i =0; i<Object.keys(alpha).length; i++){
+    for(let i =start; i<Object.keys(alpha).length; i++){
         let k = Object.keys(alpha)[i];
         if(!pick[alpha[k]]){
             pick[alpha[k]] = true;
-            find(level+1);
+            find(level+1,i+1);
             pick[alpha[k]] = false;
         }
     }
 }
 
-find(0)
+find(0,0)
 
 console.log(max)
